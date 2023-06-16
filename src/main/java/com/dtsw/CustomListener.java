3f28c7669c82dab99aa10d85d2dd26b7a1ccb5e8
10172 <wen.xin@datasw.com>
2023-06-16 19:08:19 +0800

    初始化

---
+++ src/main/java/com/dtsw/CustomListener.java
@@ -0,0 +1,45 @@
+package com.dtsw;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hive.metastore.MetaStoreEventListener;
+import org.apache.hadoop.hive.metastore.events.AlterTableEvent;
+import org.apache.hadoop.hive.metastore.events.CreateTableEvent;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class CustomListener extends MetaStoreEventListener {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomListener.class);
+    private static final  ObjectMapper objMapper = new ObjectMapper();
+
+    public CustomListener(Configuration config) {
+        super(config);
+        logWithHeader(" created ");
+    }
+
+    @Override
+    public void onCreateTable(CreateTableEvent event) {
+        logWithHeader(event.getTable());
+    }
+
+    @Override
+    public void onAlterTable(AlterTableEvent event) {
+        logWithHeader(event.getOldTable());
+        logWithHeader(event.getNewTable());
+    }
+
+    private void logWithHeader(Object obj){
+        LOGGER.info("[CustomListener][Thread: " + Thread.currentThread().getName()+"] | " + objToStr(obj));
+    }
+
+    private String objToStr(Object obj){
+        try {
+            return objMapper.writeValueAsString(obj);
+        } catch (IOException e) {
+            LOGGER.error("Error on conversion", e);
+        }
+        return null;
+    }
+}
